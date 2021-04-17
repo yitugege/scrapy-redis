bb6d293c376f81336d8274d344353ea3d04658b6
yitugege <sam.zxj@qq.com>
2021-04-17 15:56:08 -0500

    修复

--- scrapy_redis_test/proxy.py
+++ scrapy_redis_test/proxy.py
@@ -1,10 +1,11 @@
 import random
 from scrapy.exceptions import CloseSpider
+from .proxyip import PROXIES
 class ProxyMiddleware(object):
     #def process_request(self, request, spider):
     #    request.meta['proxy'] = 'http://107.173.122.99:8888'
     def process_request(self, request, spider):
-        ip = random.choice(spider.settings.get('PROXIES'))
+        ip = random.choice(PROXIES)
         #ip = random.randint(8,254)
         #ip = "http://23.231.106."+str(ip)+":8888"   
         #print('测试IP:', ip)
